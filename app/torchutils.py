98f8470f369697d1e50c362af7db0f18c71e5a1a
ClementMillet <clement7498@outlook.com>
2021-06-22 13:44:20 +0200

    adding other results when no rock is detected

--- app/torchutils.py
+++ app/torchutils.py
@@ -5,6 +5,7 @@ import torchvision
 from torchvision import models
 import torchvision.transforms as transforms
 import numpy as np
+import csv
 from PIL import Image
 import io
 import copy
@@ -13,12 +14,20 @@ PATH = "model.pth"
 
 input_size = 224
 
+imagenet_class = []
+
+with open('imagenet_class.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        imagenet_class.append(row)
+
 model = models.resnet18(pretrained=True)
 model_ori = copy.deepcopy(model)
 num_ftrs = model.fc.in_features
 model.fc = nn.Linear(num_ftrs, 2)
 model.load_state_dict(torch.load(PATH, map_location=torch.device('cpu')))
 model.eval()
+model_ori.eval()
 
 def transform_image(image):
     image = image.convert('RGB')
@@ -34,8 +43,8 @@ def get_prediction(image_tensor):
     output = model(image_tensor)
     _, predicted = torch.max(output.data, 1)
     if predicted == 1:
-        return ['rock', output.data]
+        return ['rock', torch.max(output.data)]
     else:
         outputs = model_ori(image_tensor)
         _,predicted = torch.max(outputs.data, 1)
-        return [predicted, outputs.name]
+        return [imagenet_class[predicted][0], torch.max(outputs.data)]
